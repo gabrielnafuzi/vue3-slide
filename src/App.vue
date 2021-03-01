1e2d656cfcc45cc9d788ed19b9b904fb45e2a960
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-03-01 14:14:37 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <VueSlide />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import VueSlide from './components/VueSlide.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    VueSlide
+  }
+})
+</script>
---
+++ src/components/VueSlide.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="imagePath in imagesPath" :key="imagePath">
+        <img :src="imagePath" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'VueSlide',
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  setup: () => {
+    const imagesPath: Array<string> = [
+      'src/assets/img/foto1.jpg',
+      'src/assets/img/foto2.jpg',
+      'src/assets/img/foto3.jpg',
+      'src/assets/img/foto4.jpg',
+      'src/assets/img/foto5.jpg',
+      'src/assets/img/foto6.jpg'
+    ]
+
+    return {
+      imagesPath
+    }
+  }
+})
+</script>
